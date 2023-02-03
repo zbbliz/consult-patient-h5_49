9506f879e9775aabb0b5ba1807a6967fec558b4d
liz <2931325376@qq.com>
2023-02-03 14:30:22 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true, // 单引号
+        semi: false, // 没有分号
+        printWidth: 80, // 单行宽度
+        trailingComma: 'none', // 没有对象数组最后一个逗号
+        endOfLine: 'auto' // 换行字符串自动（系统不一样换行符号不一样）
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index']
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off']
+  }
+}
